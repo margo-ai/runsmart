900dac1ef1b4d1bde826d9cfe9911d016febb0a1
Margo <plokhova.margarita@yandex.ru>
2021-08-13 18:15:02 +0300

    create slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 
 $(document).ready(function(){
-    $('.carousel__inner').slick({        
-        infinite: true,
+    $('.carousel__inner').slick({     
         speed: 1200,
-        adaptiveHeight: true
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/next.svg"></button>'
       });
   });
