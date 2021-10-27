9fd44215ec50d7120c943293571d427c1addc411
Margo <plokhova.margarita@yandex.ru>
2021-10-27 15:39:54 +0300

    create scrolling button

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,12 +13,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'plokhova.margarita';                 // Наш логин
+$mail->Username = 'plokhova.margarita@gmail.com';                 // Наш логин
 $mail->Password = 'taisetsunakizuna4life';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('plokhova.margarita', 'Pulse');   // От кого письмо 
+$mail->setFrom('plokhova.margarita@gmail.com', 'Pulse');   // От кого письмо 
 $mail->addAddress('plokhova.margarita@yandex.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
